322e3947275389b4364a58a2c21247f5f3310294
ProjectApp <akshaynikum1488@gmail.com>
2025-05-26 12:20:39 +0530

    Initial commit

---
+++ Spring Boot/demo/src/main/java/com/zpms/demo/ZillaParshadManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zpms.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZillaParshadManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZillaParshadManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Spring Boot/demo/src/test/java/com/zpms/demo/ZillaParshadManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zpms.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZillaParshadManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
